8a0c933fe61f5181e684b9150d7d741bf412d8c5
krishna tyagi <35443292+krishtyagi277@users.noreply.github.com>
2020-11-12 12:19:04 +0530

    Update homePage.js

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,10 +1,11 @@
-const Post =  require("../database/models/Post")
+//const Post =  require("../database/models/Post")
 
 module.exports  = async (req, res)=>{
   console.log(req.session)
-        const posts = await Post.find({});
+        c//onst posts = await Post.find({});
         
-        res.render('index', {
-          posts
-        });
+//         res.render('index', {
+//           posts
+//         });
+        res.render('index')
         }
