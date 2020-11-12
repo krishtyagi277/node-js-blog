e2b6b04e9c3f1284e9a432d441ec5f4aaebe3aff
krishna tyagi <35443292+krishtyagi277@users.noreply.github.com>
2020-11-12 12:19:19 +0530

    Update homePage.js

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -2,7 +2,7 @@
 
 module.exports  = async (req, res)=>{
   console.log(req.session)
-        c//onst posts = await Post.find({});
+        //const posts = await Post.find({});
         
 //         res.render('index', {
 //           posts
